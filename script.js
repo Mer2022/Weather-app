007637b3fad590b5ceada4b22e3cfb1212907e83
Mer2022 <112989286+Mer2022@users.noreply.github.com>
2023-02-13 17:56:05 -0600

    established variable to store searched city

---
+++ script.js
@@ -0,0 +1,12 @@
+
+//Variable declaration to save the searched city and related weather information
+var city="";
+var searchButton = $("#search-button");
+var searchCity = $("#search-city");
+var currentCity = $("#current-city");
+var clearButton = $("#clear-history");
+var currentTemperature = $("#temperature");
+var currentUvindex = $("#uv-index");
+var currentWSpeed = $("#wind-speed");
+var currentHumidity = $("#humidity");
+var sCity=[];
