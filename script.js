1c27d86c2c67ab29696ba0d23998bdd684f9c751
Mer2022 <112989286+Mer2022@users.noreply.github.com>
2023-02-28 19:30:50 -0600

    added temperature and humidity

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-var APIKey="e7df9921dfd1d5f5d129ce1cd666c3b7";
 
+
+var APIKey="e7df9921dfd1d5f5d129ce1cd666c3b7";
+var searchButton=document.getElementById("search-button")
+var cityName=document.getElementById("current-city")
+var searchCity=document.getElementById("search-city")
+var temperature=document.getElementById("temperature")
+var humidity=document.getElementById("humidity")
 
     function fetchWeather(city){
-        var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+        var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=" + APIKey;
 
         fetch(queryURL)
         .then(function (res) {
@@ -10,9 +16,14 @@ var APIKey="e7df9921dfd1d5f5d129ce1cd666c3b7";
         })
         .then(function (data) {
          console.log(data)
+         cityName.textContent=data.name
+         temperature.textContent=data.main.temp
+         humidity.textContent=data.main.humidity
+        
         })
         .catch(function (err) {
           console.error(err);
         });
     }
-fetchWeather("dallas")
+// fetchWeather("dallas")
+searchButton.addEventListener("click", function(){fetchWeather(searchCity.value)})
